09358d85c57e715e42228a6b624b192327735354
hopelessfree <781154248@qq.com>
2022-07-15 16:25:53 +0800

    线上发布

--- deploy.config.js
+++ deploy.config.js
@@ -17,7 +17,7 @@ module.exports = {
 			user: 'root',
 			host: '47.96.134.225',
 			ref: 'origin/master',
-			repo: 'https://github.com/hopelessfree/txclass-manager.git',
+			repo: 'https://github.com/hopelessfree/txclass-crawler.git',
 			path: '/www/txclass-crawler/production',
 			'pre-deploy': 'git fetch --all',
 			'post-deploy': 'npm install && npm run prd && pm2 startOrRestart deploy.config.js --env production'
