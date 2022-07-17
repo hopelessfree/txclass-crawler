8ed1045c21ecb0fe921b1fd766f810205cd72baa
hopelessfree <781154248@qq.com>
2022-07-17 19:26:43 +0800

    线上发布

--- deploy.config.js
+++ deploy.config.js
@@ -20,7 +20,7 @@ module.exports = {
 			repo: 'https://github.com/hopelessfree/txclass-crawler.git',
 			path: '/www/txclass-crawler/production',
 			'pre-deploy': 'git fetch --all',
-			'post-deploy': 'yarn && yarn prd && pm2 startOrRestart deploy.config.js --env production'
+			'post-deploy': 'yarn install && yarn prd && pm2 startOrRestart deploy.config.js --env production'
 		}
 	}
 }
