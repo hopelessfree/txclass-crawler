a7c8bc5c061a749288c87c134a7a41bddb867ffe
hopelessfree <781154248@qq.com>
2022-07-20 18:01:33 +0800

    done

--- deploy.config.js
+++ deploy.config.js
@@ -18,8 +18,8 @@ module.exports = {
 			host: '47.96.134.225',
 			ref: 'origin/master',
 			repo: 'https://github.com/hopelessfree/txclass-crawler.git',
-			ssh_options: "StrictHostKeyChecking=no",
 			path: '/www/txclass-crawler/production',
+			ssh_options: "StrictHostKeyChecking=no",
 			'pre-deploy': 'git fetch --all',
 			'post-deploy': 'yarn install && yarn prd && pm2 startOrRestart deploy.config.js --env production'
 		}
